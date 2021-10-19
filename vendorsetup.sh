af72f6161f07cb0b2ecbfbe6bb8cb3eda4b32227
BayerischeMotorenWerke <syaifulamri199@gmail.com>
2021-10-20 01:38:50 +0700

    tissot: Clone hal
    Signed-off-by: BayerischeMotorenWerke <syaifulamri199@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -8,3 +8,9 @@ git clone https://github.com/Gabuters-Dev/gabuters-clang $gclang
 else
 echo "Gabuters clang exist, skipping.."
 fi
+
+rm -rf hardware/qcom-caf/msm8996/{audio,media,display}
+git clone https://github.com/ItsVixano/hardware_qcom-caf -b display-msm8996-s hardware/qcom-caf/msm8996/display
+git clone https://github.com/glassydan/hardware_qcom-caf_msm8996-r_media -b eleven hardware/qcom-caf/msm8996/media
+git clone https://github.com/glassydan/android_hardware_qcom_audio -b lineage-18.1-caf-msm8996 hardware/qcom-caf/msm8996/audio
+cd hardware/qcom-caf/msm8996/display; git revert 3278ed4a4d3646238702404816377abfa27ed91c
